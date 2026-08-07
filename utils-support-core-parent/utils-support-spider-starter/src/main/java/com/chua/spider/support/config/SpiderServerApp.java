5aaecb625ecb5d7961b11912f362e4014590cbec
ACHTK <yemengzhidiandig@gmail.com>
2026-08-07 21:37:45 +0800

    feat/spider: 独立启动入口 SpiderServerApp + Spring 版本兼容

---
+++ utils-support-core-parent/utils-support-spider-starter/src/main/java/com/chua/spider/support/config/SpiderServerApp.java
@@ -0,0 +1,27 @@
+package com.chua.spider.support.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 爬虫中心独立服务启动入口。
+ *
+ * <p>供本地联调使用，监听 19091 端口暴露 SpiderController / SpiderProxyPoolController 接口。</p>
+ *
+ * @author CH
+ * @since 4.0.0.42
+ */
+@SpringBootApplication
+public class SpiderServerApp {
+
+    public static void main(String[] args) {
+        System.setProperty("logging.config", "");
+        SpringApplication app = new SpringApplication(SpiderServerApp.class);
+        app.setDefaultProperties(java.util.Map.of(
+                "server.port", 19091,
+                "spring.main.banner-mode", "off",
+                "logging.level.root", "INFO"
+        ));
+        app.run(args);
+    }
+}
