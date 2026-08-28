fda2b7ae7730085910821e389b9c38224250c8f1
CH <ch@chua.com>
2026-08-29 07:15:22 +0800

    chore: remove temp test files from commit

--- utils-support-datatask-parent/utils-support-datasearch-starter/CheckLoad2.java
+++
@@ -1,7 +0,0 @@
-import com.chua.common.support.datasearch.video.spi.VideoProviderRegistry;
-public class CheckLoad2 {
-    public static void main(String[] a) {
-        VideoProviderRegistry.initBlockedResources();
-        System.out.println("count: " + VideoProviderRegistry.getBlockedNames().size());
-    }
-}
