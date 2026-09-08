86a77bfb1800ba9909453d2a42f9082189da7356
CH <ch@chua.com>
2026-09-08 20:18:13 +0800

    chore: agent updates

--- utils-support-remote-parent/utils-support-remote-gateway/src/main/java/com/chua/remote/gateway/GatewayServer.java
+++ utils-support-remote-parent/utils-support-remote-gateway/src/main/java/com/chua/remote/gateway/GatewayServer.java
@@ -504,10 +504,13 @@ public class GatewayServer implements RemoteServerSPI {
         ServerSetting setting = ServerSetting.builder().port(port).build();
         GatewayServer server = new GatewayServer(setting);
         server.start();
-        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
-            log.error("未捕获异常 thread={} name={}", t.getName(), t.getId(), e);
-        });
-        Thread.currentThread().join();
+        Thread.setDefaultUncaughtExceptionHandler((t, e) ->
+                log.error("未捕获异常 thread={}", t.getName(), e));
+        try {
+            Thread.currentThread().join();
+        } catch (InterruptedException ignored) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
 
