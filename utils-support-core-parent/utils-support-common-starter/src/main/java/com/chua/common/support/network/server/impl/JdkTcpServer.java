14a9417457b2be60bca277d1fecb2f7e8391af6e
CH <ch@chua.com>
2026-09-08 08:07:22 +0800

    chore: agent updates

--- utils-support-core-parent/utils-support-common-starter/src/main/java/com/chua/common/support/network/server/impl/JdkTcpServer.java
+++ utils-support-core-parent/utils-support-common-starter/src/main/java/com/chua/common/support/network/server/impl/JdkTcpServer.java
@@ -512,6 +512,7 @@ public class JdkTcpServer extends AbstractServer implements TcpServer {
             return;
         }
         // URL 路由模式：将帧体解析为 HTTP 请求，走完整 Filter Chain
+        System.err.println("[JdkTcpServer.URL_PATH] entering URL routing mode");
         InetSocketAddress remoteAddr = null;
         try {
             java.net.Socket socket = sc.socket();
