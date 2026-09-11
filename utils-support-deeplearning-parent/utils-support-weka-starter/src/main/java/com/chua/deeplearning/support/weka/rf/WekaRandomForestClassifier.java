7b4f52b3d06ea61419d68726ae1d2edde305926d
CH <ch@chua.com>
2026-09-12 07:15:28 +0800

    chore: weka forest classifier update

--- utils-support-deeplearning-parent/utils-support-weka-starter/src/main/java/com/chua/deeplearning/support/weka/rf/WekaRandomForestClassifier.java
+++ utils-support-deeplearning-parent/utils-support-weka-starter/src/main/java/com/chua/deeplearning/support/weka/rf/WekaRandomForestClassifier.java
@@ -16,6 +16,7 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Random;
 import weka.classifiers.evaluation.Evaluation;
 
 /**
