6bc70c7e89804a626281d2649a9d49b7f9913013
CH <ch@chua.com>
2026-09-11 09:51:44 +0800

    chore: update felix bundle context

--- utils-support-core-parent/utils-support-osgi-starter/src/main/java/com/chua/osgi/support/FelixBundleContext.java
+++ utils-support-core-parent/utils-support-osgi-starter/src/main/java/com/chua/osgi/support/FelixBundleContext.java
@@ -3,6 +3,8 @@ package com.chua.osgi.support;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Felix BundleContext 实现，包装 Felix 框架的 BundleContext。
@@ -18,6 +20,14 @@ public class FelixBundleContext implements com.chua.common.support.osgi.BundleCo
     private final org.osgi.framework.BundleContext delegate;
 
     /**
+     * 注册跟踪表：ServiceReference → ServiceRegistration。
+     * <p>{@code unregisterService} 需经 {@link org.osgi.framework.ServiceRegistration#unregister()}
+     * 真正注销服务（仅 ungetService 只释放引用计数，服务仍留在注册表）。</p>
+     */
+    private final Map<org.osgi.framework.ServiceReference<?>, org.osgi.framework.ServiceRegistration<?>> registrations =
+            new ConcurrentHashMap<>();
+
+    /**
      * 构造函数。
      *
      * @param delegate OSGI BundleContext
@@ -29,12 +39,23 @@ public class FelixBundleContext implements com.chua.common.support.osgi.BundleCo
     @Override
     /** 注册Service */
     public <T> void registerService(Class<T> type, T service) {
-        delegate.registerService(type.getName(), service, null);
+        org.osgi.framework.ServiceRegistration<?> reg =
+                delegate.registerService(type.getName(), service, null);
+        registrations.put(reg.getReference(), reg);
     }
 
     @Override
     /** 注销Service */
     public <T> void unregisterService(Class<T> type, T service) {
+        for (Map.Entry<org.osgi.framework.ServiceReference<?>, org.osgi.framework.ServiceRegistration<?>> e
+                : registrations.entrySet()) {
+            Object svc = delegate.getService(e.getKey());
+            if (svc == service) {
+                e.getValue().unregister();
+                registrations.remove(e.getKey());
+                return;
+            }
+        }
         try {
             org.osgi.framework.ServiceReference<?>[] refs =
                     delegate.getServiceReferences(type.getName(), null);
